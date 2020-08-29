8ef84e4999330d2fa2bb56c4e9e646e448e7848c
KirillEmets <kirill.yemets@gmail.com>
2020-08-29 11:36:41 +0300

    added navigation within fragments

--- app/src/main/java/com/kirillemets/flashcards/MainActivity.kt
+++ app/src/main/java/com/kirillemets/flashcards/MainActivity.kt
@@ -3,11 +3,9 @@ package com.kirillemets.flashcards
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.kirillemets.flashcards.network.JishoApi
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+
 
 lateinit var bottomNavigation: BottomNavigationView
 
@@ -20,18 +18,23 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigation = findViewById(R.id.bottom_navigation)
 
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            if(bottomNavigation.selectedItemId == item.itemId)
+                return@setOnNavigationItemSelectedListener false
             when(item.itemId) {
                 R.id.page_1 -> {
                     Log.i("HELLO", "1")
                     true
                 }
                 R.id.page_2 -> {
-                    Log.i("HELLO", "2")
+                    navController.navigate(R.id.action_global_addWordFragment)
                     true
                 }
                 R.id.page_3 -> {
-                    Log.i("HELLO", "3")
+                    navController.navigate(R.id.action_global_myDictionaryFragment)
                     true
                 }
                 else -> false
