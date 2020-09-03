b9748d8acd8cbe783bb03f14b6336f62254cecb9
KirillEmets <kirill.yemets@gmail.com>
2020-09-03 09:37:13 +0300

    added getRemainingTime to flashCard and lastRepeat to item_dictionary_flashcard.xml

--- app/src/main/java/com/kirillemets/flashcards/database/FlashCard.kt
+++ app/src/main/java/com/kirillemets/flashcards/database/FlashCard.kt
@@ -3,9 +3,11 @@ package com.kirillemets.flashcards.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 @Entity
 data class FlashCard(
@@ -16,4 +18,11 @@ data class FlashCard(
     @ColumnInfo(name = "last_delay") val lastDelay: Int = 1,
     @ColumnInfo(name = "last_repeat_date") val lastRepeatDate: String =
         SimpleDateFormat("dd-MM-yyyy", Locale.US).format(GregorianCalendar().time)
-)
+) {
+    fun getRemainingTime(currentDate: Long = GregorianCalendar().time.time): Int {
+        val lastRepeatDate: Long = SimpleDateFormat("dd-MM-yyyy", Locale.US).parse(lastRepeatDate)?.time ?: 0
+
+        return (lastDelay - TimeUnit.DAYS.convert(currentDate - lastRepeatDate, TimeUnit.MILLISECONDS)
+            .toInt()).coerceAtLeast(0)
+    }
+}
