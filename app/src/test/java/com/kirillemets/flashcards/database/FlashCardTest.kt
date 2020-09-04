24d3a2fe76e3722349f4389c78d624721eea2951
KirillEmets <kirill.yemets@gmail.com>
2020-09-04 16:46:27 +0300

    typo fix убрал лишнее присвоение

--- app/src/test/java/com/kirillemets/flashcards/database/FlashCardTest.kt
+++ app/src/test/java/com/kirillemets/flashcards/database/FlashCardTest.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-data class RemainingTimeTestData(val lastDelay: Int, val lastRepeatDateString: String, val currentTimeString: String, val expextedValue: Int)
+data class RemainingTimeTestData(val lastDelay: Int, val lastRepeatDateString: String, val currentTimeString: String, val expectedValue: Int)
 class FlashCardTest {
     private val tests = listOf(
         RemainingTimeTestData(4, "01-06-2020", "05-06-2020", 0),
@@ -19,7 +19,7 @@ class FlashCardTest {
     @Test
     fun getRemainingTime() {
         var card: FlashCard
-        var lastRepeatTime: Long = 0
+        var lastRepeatTime: Long
 
         tests.forEachIndexed { i, test ->
             lastRepeatTime = SimpleDateFormat("dd-MM-yyyy", Locale.US).parse(test.lastRepeatDateString)?.time ?: 0
@@ -29,7 +29,7 @@ class FlashCardTest {
                 SimpleDateFormat("dd-MM-yyyy", Locale.US).parse(test.currentTimeString)?.time ?: 0
 
             val remaining: Int = card.getRemainingTime(currentTime)
-            assertEquals("test $i:", test.expextedValue, remaining)
+            assertEquals("test $i:", test.expectedValue, remaining)
         }
     }
 }
