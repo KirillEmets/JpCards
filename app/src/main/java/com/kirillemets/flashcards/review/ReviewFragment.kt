c7b87856cbf35279b63aa35510bd4dd4a08f04b5
KirillEmets <kirill.yemets@gmail.com>
2021-06-03 09:16:03 +0300

    Use preferences for multipliers use viewLifecycleOwner

--- app/src/main/java/com/kirillemets/flashcards/review/ReviewFragment.kt
+++ app/src/main/java/com/kirillemets/flashcards/review/ReviewFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceManager
 import com.kirillemets.flashcards.R
 import com.kirillemets.flashcards.database.CardDatabase
 import com.kirillemets.flashcards.databinding.FragmentReviewBinding
 import com.kirillemets.flashcards.myDictionary.MyDictionaryFragmentViewModelFactory
 
 class ReviewFragment : Fragment() {
-
     private lateinit var viewModel: ReviewFragmentViewModel
 
     override fun onCreateView(
@@ -24,28 +24,34 @@ class ReviewFragment : Fragment() {
         val database = CardDatabase.getInstance(requireContext()).flashCardsDao()
         val binding = FragmentReviewBinding.inflate(inflater)
 
-
         viewModel = ViewModelProvider(
             this,
             MyDictionaryFragmentViewModelFactory(database)
         ).get(ReviewFragmentViewModel::class.java)
 
+        val preferenceManager = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        viewModel.delayHardMultiplier = preferenceManager.getFloat("hard_multiplier", 1f)
+        viewModel.delayEasyMultiplier = preferenceManager.getFloat("easy_multiplier", 1f)
+
+        val size = preferenceManager.getFloat("review_font_size", 30f).toInt()
+        viewModel.fontSizeBig.value = (size * resources.displayMetrics.scaledDensity).toInt()
+        viewModel.fontSizeSmall.value = ((size - 12) * resources.displayMetrics.scaledDensity).toInt()
+
+
         binding.viewModel = viewModel
 
-
-        viewModel.reviewCards.observe(this, {
+        viewModel.reviewCards.observe(viewLifecycleOwner, {
             binding.countOfCardsTextView.text = resources.getString(R.string.countOfWordsToReview, it.size)
         })
 
-        viewModel.currentCard.observe(this, {
-            binding.easyButtonDelay.text = resources.getQuantityString(R.plurals.daysToDelay, ReviewFragmentViewModel.getNewDelay(it.lastDelay, 1), ReviewFragmentViewModel.getNewDelay(it.lastDelay, 1))
-            binding.hardButtonDelay.text = resources.getQuantityString(R.plurals.daysToDelay, ReviewFragmentViewModel.getNewDelay(it.lastDelay, 2), ReviewFragmentViewModel.getNewDelay(it.lastDelay, 2))
+        viewModel.currentCard.observe(viewLifecycleOwner, {
+            binding.easyButtonDelay.text = resources.getQuantityString(R.plurals.daysToDelay, viewModel.getNewDelay(it.lastDelay, 1), viewModel.getNewDelay(it.lastDelay, 1))
+            binding.hardButtonDelay.text = resources.getQuantityString(R.plurals.daysToDelay, viewModel.getNewDelay(it.lastDelay, 2), viewModel.getNewDelay(it.lastDelay, 2))
         })
 
-        viewModel.onRunOutOfWords.observe(this, {
+        viewModel.onRunOutOfWords.observe(viewLifecycleOwner, {
             if(it) {
                 viewModel.onRunOutOfWords.value = false
-
                 findNavController().navigate(R.id.action_global_reviewFragment)
             }
         })
