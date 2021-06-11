1ad1cb9ecfced6060e27d1052d3733ef26f7ee27
KirillEmets <kirill.yemets@gmail.com>
2021-06-11 14:12:14 +0300

    wtf3 Revert "use SearchView in AddWordFragment" This reverts commit 4962be0c

--- app/src/main/java/com/kirillemets/flashcards/addWord/AddWordFragment.kt
+++ app/src/main/java/com/kirillemets/flashcards/addWord/AddWordFragment.kt
@@ -1,21 +1,22 @@
 package com.kirillemets.flashcards.addWord
 
 import android.os.Bundle
-import android.util.Log
-import android.view.*
-import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
-import com.kirillemets.flashcards.R
 import com.kirillemets.flashcards.database.CardDatabase
 import com.kirillemets.flashcards.databinding.FragmentAddWordBinding
 
 class AddWordFragment : Fragment() {
+
+
     private val viewModel: AddWordFragmentViewModel by lazy {
         ViewModelProvider(this).get(AddWordFragmentViewModel::class.java)
     }
-
-    lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +24,10 @@ class AddWordFragment : Fragment() {
     ): View {
         val binding = FragmentAddWordBinding.inflate(inflater)
         viewModel.database = CardDatabase.getInstance(requireContext()).flashCardsDao()
-        val adapter =
-            AddWordFragmentAdapter(AddWordFragmentAdapter.AddWordFragmentAdapterCallback { searchResult, id ->
-                viewModel.onAddButtonClicked(searchResult, id)
-            })
+        val adapter = AddWordFragmentAdapter(AddWordFragmentAdapter.AddWordFragmentAdapterCallback {
+            searchResult, id ->
+            viewModel.onAddButtonClicked(searchResult, id)
+        })
 
         binding.recyclerView.adapter = adapter
         binding.lifecycleOwner = this
@@ -34,40 +35,12 @@ class AddWordFragment : Fragment() {
         viewModel.flashCards.observe(viewLifecycleOwner, { list ->
             adapter.searchResultCards = list
         })
-        setHasOptionsMenu(true)
+
+        binding.textField.editText?.doAfterTextChanged {
+            if(!it.isNullOrBlank())
+                viewModel.startSearch(it.toString())
+        }
 
         return binding.root
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.add_word_menu, menu)
-        (menu.findItem(R.id.item_search_add_word)?.actionView as SearchView).apply {
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    if (query.isBlank())
-                        return true
-
-                    viewModel.startSearch(query)
-                    return true
-                }
-
-                override fun onQueryTextChange(newText: String): Boolean {
-                    if (newText.isBlank())
-                        return true
-
-                    viewModel.startSearch(newText, false)
-                    return true
-                }
-            })
-        }
-    }
-
-    var clearQuery = true
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        if(clearQuery) {
-            (menu.findItem(R.id.item_search_add_word)?.actionView as SearchView)
-                .setQuery("", false)
-            clearQuery = false
-        }
     }
 }
