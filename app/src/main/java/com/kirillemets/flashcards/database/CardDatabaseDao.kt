f9d3152fab9397f9f44d6dce0de7bf26d5d54390
KirillEmets <kirill.yemets@gmail.com>
2021-06-11 14:07:51 +0300

    Add getAllBlocking functions for test

--- app/src/main/java/com/kirillemets/flashcards/database/CardDatabaseDao.kt
+++ app/src/main/java/com/kirillemets/flashcards/database/CardDatabaseDao.kt
@@ -8,6 +8,9 @@ interface CardDatabaseDao {
     @Query("SELECT * FROM flashcard ORDER BY cardId DESC")
     fun getAll(): LiveData<List<FlashCard>>
 
+    @Query("SELECT * FROM flashcard ORDER BY cardId DESC")
+    fun getAllBlocking(): List<FlashCard>
+
     @Query("SELECT * FROM flashcard WHERE cardId = :id")
     fun get(id: Int): FlashCard?
 
