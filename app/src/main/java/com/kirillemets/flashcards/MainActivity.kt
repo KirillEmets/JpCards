27656d6d10c72f46c35a66801c20d146a06d2b12
KirillEmets <kirill.yemets@gmail.com>
2021-06-11 11:39:56 +0300

    Add theme choice: auto, light or black

--- app/src/main/java/com/kirillemets/flashcards/MainActivity.kt
+++ app/src/main/java/com/kirillemets/flashcards/MainActivity.kt
@@ -3,7 +3,9 @@ package com.kirillemets.flashcards
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
+import androidx.preference.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.danlew.android.joda.JodaTimeAndroid
 
@@ -11,11 +13,20 @@ import net.danlew.android.joda.JodaTimeAndroid
 lateinit var bottomNavigation: BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         JodaTimeAndroid.init(this)
+        val preferenceManager = PreferenceManager.getDefaultSharedPreferences(this)
+        val theme = preferenceManager.getString("theme", "Auto")
+        AppCompatDelegate.setDefaultNightMode(
+        when (theme) {
+            "Auto" -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            "Light" -> AppCompatDelegate.MODE_NIGHT_NO
+            "Black" -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        })
+
         setContentView(R.layout.activity_main)
 
         bottomNavigation = findViewById(R.id.bottom_navigation)
--- app/src/main/java/com/kirillemets/flashcards/preference/SettingsFragment.kt
+++ app/src/main/java/com/kirillemets/flashcards/preference/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.kirillemets.flashcards.preference
 
 import android.os.Bundle
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.kirillemets.flashcards.R
 
@@ -21,6 +23,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
         findPreference<MaterialSliderPreference>("review_font_size")!!.apply {
             provideFragmentManager(requireActivity().supportFragmentManager)
             setBounds(22f, 36f, 2f)
+        }
+
+        findPreference<ListPreference>("theme")!!.apply {
+            setOnPreferenceChangeListener { _: Preference, _: Any ->
+                requireActivity().finish()
+                startActivity(requireActivity().intent)
+                true
+            }
         }
     }
 }
