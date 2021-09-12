0530bfde3470ad3c1e69d56bd94b56694a9c882f
KirillEmets <kirill.yemets@gmail.com>
2021-09-12 11:06:24 +0300

    format code

--- app/src/main/java/com/kirillemets/flashcards/review/ReviewStarterFragment.kt
+++ app/src/main/java/com/kirillemets/flashcards/review/ReviewStarterFragment.kt
@@ -5,11 +5,8 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.kirillemets.flashcards.R
-import com.kirillemets.flashcards.database.CardDatabase
-import com.kirillemets.flashcards.database.FlashCardRepository
 import com.kirillemets.flashcards.databinding.FragmentReviewStarterBinding
 import dagger.hilt.android.AndroidEntryPoint
 
