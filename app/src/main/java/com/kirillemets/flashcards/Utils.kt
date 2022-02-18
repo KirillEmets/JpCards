7727669bccdcbd15e38f71345ce1c6ba6032a805
kirill <kirill.yemets@gmail.com>
2022-02-18 13:07:34 +0200

    add utils and activityResultLauncher wrapper

---
+++ app/src/main/java/com/kirillemets/flashcards/Utils.kt
@@ -0,0 +1,31 @@
+package com.kirillemets.flashcards
+
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.first
+
+class SuspendableActivityResultLauncher<I, O>(
+    private val sharedFlow: SharedFlow<O>,
+    private val launcher: ActivityResultLauncher<I>
+) {
+
+    suspend fun launchAndAwait(input: I): O {
+        launcher.launch(input)
+        return sharedFlow.first()
+    }
+}
+
+fun <I, O> Fragment.registerForSuspendableActivityResult(contract: ActivityResultContract<I, O>): SuspendableActivityResultLauncher<I, O> {
+    val mutableSharedFlow =
+        MutableSharedFlow<O>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
+    val launcher = registerForActivityResult(contract) { result ->
+        Log.d("kek", "tryEmit: ${mutableSharedFlow.tryEmit(result)}")
+    }
+    return SuspendableActivityResultLauncher(mutableSharedFlow, launcher)
+}
