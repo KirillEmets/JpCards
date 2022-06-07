5ede80ada0771bcdaab09848c262e1e5d50baaf3
KirillEmets <kirill.yemets@gmail.com>
2022-06-07 12:29:01 +0300

    Remove redundant setInstance method in CardDatabase.kt

--- app/src/main/java/com/kirillemets/flashcards/data/database/CardDatabase.kt
+++ app/src/main/java/com/kirillemets/flashcards/data/database/CardDatabase.kt
@@ -7,13 +7,5 @@ import org.jetbrains.annotations.TestOnly
 
 @Database(entities = [FlashCard::class], version = 1)
 abstract class CardDatabase: RoomDatabase() {
-    companion object {
-        private var Instance: CardDatabase? = null
-
-        @TestOnly
-        fun setInstance(database: CardDatabase) {
-            Instance = database
-        }
-    }
     abstract fun flashCardsDao(): CardDatabaseDao
 }
